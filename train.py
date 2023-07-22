a2041536db255361ea9ee089132b6141087323f1
TAPAN BISWAS <biswasdebosmita1701@gmail.com>
2023-07-22 18:12:04 +0530

    changes_made_by_Debosmita

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ emotion_model = Sequential()
 emotion_model.add(Conv2D(32,kernel_size=(3,3),activation='relu', input_shape=(48,48,1)))
 emotion_model.add(Conv2D(32,kernel_size=(3,3),activation='relu'))
 emotion_model.add(MaxPooling2D(pool_size=(2,2)))
-emotion_model.add(Dropout(0,25))
+emotion_model.add(Dropout(0.25))
 emotion_model.add(Conv2D(128,kernel_size=(3,3),activation='relu'))
 emotion_model.add(MaxPooling2D(pool_size=(2,2)))
 emotion_model.add(Conv2D(128,kernel_size=(3,3),activation='relu'))
@@ -51,3 +51,16 @@ emotion_model.add(Flatten())
 emotion_model.add(Dense(1024,activation='relu'))
 emotion_model.add(Dropout(0.5))
 emotion_model.add(Dense(7, activation='softmax'))
+
+emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
+
+emotion_model_info = emotion_model.fit_generator(
+    train_generator,
+    steps_per_epoch=28709 // 64,
+    epochs=50, #50
+    validation_data = validation_generator,
+    validation_steps=7178 // 64)
+
+
+
+emotion_model.save_weights('model.h5')
